7196ff6fce462f3674964f44eb894a6b4d3838e8
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-03-25 11:58:13 +0100

    e_some done

--- PEC1/PEC2_Ej3/e_some/core.js
+++ PEC1/PEC2_Ej3/e_some/core.js
@@ -1,29 +1,36 @@
 // Check to see if any of the elements in the
 // array are numbers greater than 10.
 
-function anyGreaterThan10 (input) {
-  return input;
+function anyGreaterThan10(input) {
+  return input.some(n => n > 10);
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
-function longWord (input) {
-  return input;
+function longWord(input) {
+  return input.some(word => word.length > 10);
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
-function truePossibilities (input) {
-  return input;
+function truePossibilities(input) {
+  return input.reduce((acc, arr, index) => {
+    if (index === 0) {
+      return arr.some(elem => elem === true);
+    }
+    else {
+      return acc || arr.some(elem => elem === true);
+    }
+  }, false);
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
-function lostCarcosa (input) {
-  return input;
+function lostCarcosa(input) {
+  return input.some(arr => arr.includes('Lost'))
 };
 
 module.exports = {
