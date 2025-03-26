8f04e0da266b74a6817eac0ee1cab92a7d731197
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-03-26 01:07:48 +0100

    done entryCalculator and schedule

--- PEC1/PEC2_Ej3/f_zoo/core.js
+++ PEC1/PEC2_Ej3/f_zoo/core.js
@@ -1,9 +1,67 @@
+const data = require('./data');
+
+function calculatePrice(person, prices) {
+  const keyPerson = Object.keys(person).toString();
+  let valueTicket = 0;
+  Object.entries(prices).forEach(key => {
+    if (key[0] === keyPerson) {
+      valueTicket = key[1] * person[keyPerson];
+    }
+  }
+  );
+  return valueTicket;
+}
+
 function entryCalculator(entrants) {
-  // your code here
+  if (entrants === undefined || entrants.length === 0) {
+    return 0;
+  }
+  else {
+    const prices = data.prices;
+    const arrAmountPers = Object.keys(entrants).map(key => ({ [key]: entrants[key] }));
+    return arrAmountPers.reduce((totalPrice, elem, index) => {
+      if (index === 0) {
+        return calculatePrice(elem, prices);
+      }
+      else {
+        return totalPrice + calculatePrice(elem, prices)
+      }
+    }, 0);
+  }
+}
+
+function itIsMonday(open, close) {
+  const isMonday = (open === 0 && close === 0) ? true : false;
+  return isMonday;
+}
+
+function mountDayText(open, close) {
+  return 'Open from ' + open.toString() + 'am until ' + (close - 12).toString() + 'pm';
 }
 
 function schedule(dayName) {
-  // your code here
+  const days = data.hours;
+  if (dayName === undefined) {
+    return Object.entries(days).reduce((daysSchedule, day, index) => {
+      const daySchedule = (itIsMonday(day[1].open, day[1].close)) ? 'CLOSED' : mountDayText(day[1].open, day[1].close);
+      if (index === 0) {
+        return { [day[0]]: daySchedule };
+      }
+      else {
+        return { ...daysSchedule, [day[0]]: daySchedule };
+      }
+    }, {});
+  }
+  else {
+    const oneDay = Object.entries(days).filter(day => day[0] === dayName);
+    if (itIsMonday(oneDay[0][1].open, oneDay[0][1].close)) {
+      return { [oneDay[0][0]]: 'CLOSED' };
+    }
+    else {
+      const daySchedule = mountDayText(oneDay[0][1].open, oneDay[0][1].close);
+      return { [oneDay[0][0]]: daySchedule };
+    }
+  }
 }
 
 function animalCount(species) {
